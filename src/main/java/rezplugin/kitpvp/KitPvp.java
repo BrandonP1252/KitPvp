e1287396e0db9ae4395c3581e1e9314fd739349d
Andrew Vadeika <57453903+andrewv517@users.noreply.github.com>
2022-10-17 17:17:49 -0400

    implement dying listener (#1)

--- src/main/java/rezplugin/kitpvp/KitPvp.java
+++ src/main/java/rezplugin/kitpvp/KitPvp.java
@@ -1,37 +1,59 @@
 package rezplugin.kitpvp;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import rezplugin.kitpvp.commands.DeleteSpawnPoint;
 import rezplugin.kitpvp.commands.KitPvpStart;
 import rezplugin.kitpvp.commands.ListSpawnPoint;
 import rezplugin.kitpvp.commands.SetSpawnPoint;
 import rezplugin.kitpvp.files.SpawnPointConfig;
-import rezplugin.kitpvp.listeners.PlayerDeathEve;
+import rezplugin.kitpvp.listeners.PlayerDamageAction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public final class KitPvp extends JavaPlugin {
 
+    public static KitPvp plugin;
+    public List<Player> playerList;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        playerList = new ArrayList<>();
+        plugin = this;
 
         // regular config
         getConfig().options().copyDefaults();
         saveConfig();
 
         // spawn points config
-        SpawnPointConfig.setup();
-        SpawnPointConfig.get().options().copyDefaults(true);
-        SpawnPointConfig.save();
+        setupSpawnPoints();
 
         // commands
+        registerCommands();
+
+        // listeners
+        registerListeners();
+    }
+
+    private void registerListeners() {
+        getServer().getPluginManager().registerEvents(new PlayerDamageAction(), this);
+    }
+
+    private void registerCommands() {
         getCommand("kpsetspawnpoint").setExecutor(new SetSpawnPoint());
         getCommand("kplistspawnpoints").setExecutor(new ListSpawnPoint());
         getCommand("kpdelspawnpoint").setExecutor(new DeleteSpawnPoint());
         getCommand("kitpvp").setExecutor(new KitPvpStart());
+    }
 
-        // listeners
-        getServer().getPluginManager().registerEvents(new PlayerDeathEve(), this);
+    private void setupSpawnPoints() {
+        SpawnPointConfig.setup();
+        SpawnPointConfig.get().options().copyDefaults(true);
+        SpawnPointConfig.save();
     }
 
     @Override
--- src/main/java/rezplugin/kitpvp/listeners/PlayerDamageAction.java
+++ src/main/java/rezplugin/kitpvp/listeners/PlayerDamageAction.java
@@ -4,34 +4,34 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
-import rezplugin.kitpvp.commands.KitPvpStart;
+import org.bukkit.event.entity.EntityDamageEvent;
+import rezplugin.kitpvp.KitPvp;
 import rezplugin.kitpvp.files.SpawnPointConfig;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class PlayerDeathEve implements Listener {
-    @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event) {
-        KitPvpStart kitPvpStart = new KitPvpStart();
-        Boolean isActive = kitPvpStart.getKitPvpActive();
-        Player player = event.getEntity().getPlayer();
+public class PlayerDamageAction implements Listener {
 
-        if (isActive) {
+    @EventHandler
+    public void onPlayerDamageEvent(EntityDamageEvent event) {
+        if (!(event.getEntity() instanceof Player)) {
+            return;
+        }
+
+        Player player = (Player) event.getEntity();
+
+        if (player.getHealth() - event.getFinalDamage() <= 0 && KitPvp.plugin.playerList.contains(player)) {
+            event.setCancelled(true);
             ArrayList<Location> spawnPoints = new ArrayList<Location>();
             spawnPoints.addAll((List<Location>) SpawnPointConfig.get().getList("spawn-point"));
             Random random = new Random();
             int spawnPointIndex = random.nextInt(spawnPoints.size());
             player.teleport(spawnPoints.get(spawnPointIndex));
             player.sendMessage("you died bruh");
+            player.setHealth(20);
         }
-        else {
-            player.sendMessage("you aint in kit pvp");
-        }
-
-
     }
 
 }
